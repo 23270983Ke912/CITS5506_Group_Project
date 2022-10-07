c85fefcb02aca377fa18ae1bd6c9dbcb6400308e
Yuanyuan Li <23185169@student.uwa.edu.au>
2022-10-07 11:35:12 +0800

    upload light sensor python code

---
+++ exec/light_sensor.py
@@ -0,0 +1,28 @@
+from PiicoDev_VEML6030 import piicoDev_VEML6030
+from time import sleep
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+pin_led = 37
+
+GPIO.setup(pin_led, GPIO.OUT)
+GPIO.output(pin_led, GPIO.HIGH)
+
+light = PiicoDev_VEML6030()
+try:
+    while True:
+        lightVal = light.read()
+        print(str(lightVal) + " lux")
+        
+        if lightVal < 15:
+            print("LED ON")
+            GPIO.output(pin_led, GPIO.LOW)
+        else:
+            print("LED OFF")
+            GPIO.output(pin_led, GPIO.HIGH)
+            
+        sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
